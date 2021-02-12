f96db4a3076ede1511739475b09bdd4291686129
andresochoa91 <andres_ochoa91@hotmail.com>
2021-02-12 13:35:17 -0800

    Setting up front-end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ logged, setLogged ] = useState(false);
+  const [ user, setUser ] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    logged ? (
+      <div>
+        <h2>Sign In</h2>
+        <form>
+          <label>Email</label>
+          <input type="email"/>
+          <label>Password</label>
+          <input type="password"/>
+          <input type="submit"/>
+        </form>
+        <h2>Sign Up</h2>
+        <form>
+          <label>Email</label>
+          <input type="email"/>
+          <label>Password</label>
+          <input type="password"/>
+          <label>Password Confirmation</label>
+          <input type="password"/>
+          <input type="submit"/>
+        </form>
+      </div>
+    ) : (
+      <p>Hello { user.email }</p>
+    )
   );
 }
 
